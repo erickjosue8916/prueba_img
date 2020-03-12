362fc78bbb67a3f2fe5c9f3544cec3859ec76df1
Erick Saravia <erickjosue8916@gmail.com>
2020-03-11 19:53:07 -0600

    Redirect to image

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-    res.send('hello')
+app.get('/', async (req, res) => {
+
+    res.format({
+        'image/jpg': async function() {
+            res.redirect('https://www.appsheet.com/get/?i=YXBwTmFtZT1UQ0VQcm9qZWN0cy1UZXN0LTEzMDgyMjAmVGFibGVOYW1lPUV4cGVuc2VzTGluZSZmaWxlTmFtZT1FeHBlbnNlc0xpbmVfSW1hZ2VzJTJGZjFmZDlmMWUuUmVjZWlwdC4yMjM4MTEuanBnJmFwcFZlcnNpb249MS4wMDAxMjI=&w=200');
+        }
+    })
 })
 
 app.listen(port, console.log(`Server running on port ${port}`))
