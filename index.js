968b6c36c9f1fc3aeb6494f464ed16be142937a4
Erick Saravia <erickjosue8916@gmail.com>
2020-03-11 20:02:24 -0600

    adding post endpoint

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', async (req, res) => {
-
+    res.format({
+        'image/jpg': async function() {
+            res.redirect('https://www.appsheet.com/get/?i=YXBwTmFtZT1UQ0VQcm9qZWN0cy1UZXN0LTEzMDgyMjAmVGFibGVOYW1lPUV4cGVuc2VzTGluZSZmaWxlTmFtZT1FeHBlbnNlc0xpbmVfSW1hZ2VzJTJGZjFmZDlmMWUuUmVjZWlwdC4yMjM4MTEuanBnJmFwcFZlcnNpb249MS4wMDAxMjI=&w=200');
+        }
+    })
+})
+app.post('/', async (req, res) => {
     res.format({
         'image/jpg': async function() {
             res.redirect('https://www.appsheet.com/get/?i=YXBwTmFtZT1UQ0VQcm9qZWN0cy1UZXN0LTEzMDgyMjAmVGFibGVOYW1lPUV4cGVuc2VzTGluZSZmaWxlTmFtZT1FeHBlbnNlc0xpbmVfSW1hZ2VzJTJGZjFmZDlmMWUuUmVjZWlwdC4yMjM4MTEuanBnJmFwcFZlcnNpb249MS4wMDAxMjI=&w=200');
